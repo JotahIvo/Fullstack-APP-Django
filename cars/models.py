72098f1df41c15c0ec115376efc02c0a1ccb8a33
jota <j.pedro1415@ufu.br>
2024-03-25 20:47:35 -0300

    Creating templates and making queries with DjangoORM

--- cars/models.py
+++ cars/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+#django models
 class Brand(models.Model):
     id      = models.AutoField(primary_key=True)
     name    = models.CharField(max_length=200)
--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from cars.models import Car
 
-
+#django ORM and querysets https://docs.djangoproject.com/pt-br/4.2/topics/db/
 def cars_view(request):
+    search = request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+    else:
+        cars = Car.objects.all().order_by('model')
+
+    print(cars)
+
     return render(
         request, 
         'cars.html', 
-        {'cars': {'model': 'Astra 2.0'}}
+        {'cars': cars}
     )
