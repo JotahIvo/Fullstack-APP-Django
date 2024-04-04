ac49a2cdf3141f0e8eb4d948b4e3836631656bd4
jota <j.pedro1415@ufu.br>
2024-04-04 19:32:53 -0300

    Refactoring the functions based views to class based views

--- cars/views.py
+++ cars/views.py
@@ -1,41 +1,26 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views.generic import ListView, CreateView
+
 
 #django ORM and querysets https://docs.djangoproject.com/pt-br/4.2/topics/db/
-def cars_view(request):
-    search = request.GET.get('search')
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
-        
-        if not cars:
-            cars = Car.objects.filter(brand__name__icontains=search).order_by('model')
-    else:
-        cars = Car.objects.all().order_by('model')
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
 
-    return render(
-        request, 
-        'cars.html', 
-        {'cars': cars}
-    )
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
 
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES) #image
-        
-        if new_car_form.is_valid():
-            new_car_form.save() #method function at forms.py
-            return redirect('cars_list')
-        else:
-            return render(
-                request,
-                'new_car.html',
-                {'new_car_form': new_car_form}
-            )
-    
-    if request.method == 'GET':
+""" class NewCarView(View):
+    def get(self, request):
         new_car_form = CarModelForm()
 
         return render(
@@ -43,3 +28,23 @@ def new_car_view(request):
             'new_car.html',
             {'new_car_form': new_car_form}
         )
+    
+    def post(self, request):
+        new_car_form = CarModelForm(request.POST, request.FILES) #image
+        
+        if new_car_form.is_valid():
+            new_car_form.save() #method function at forms.py
+            return redirect('cars_list')
+    
+        return render(
+            request,
+            'new_car.html',
+            {'new_car_form': new_car_form}
+        ) """
+    
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
